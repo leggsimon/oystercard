21b04579176732e3d44f8b02e53650e42c87a356
Simon Legg <leggsimon@gmail.com>
2015-11-01 22:08:24 +0000

    tube data now from JSON

--- lib/tube_station.rb
+++ lib/tube_station.rb
@@ -1,13 +1,13 @@
+require "json"
+
 class TubeStation
-  STATION_ZONES = {
-    cannon_street: 1,
-    aldgate_east: 1
-  }
+  STATION_ZONES = JSON.parse(File.read("./tube_stations.json"))
+
 
   attr_reader :zone
 
   def initialize(station)
-    fail "This station doesn't exist!" unless STATION_ZONES.include? station
-    @zone = STATION_ZONES[station]
+    fail "This station doesn't exist!" unless STATION_ZONES.include? station.to_s
+    @zone = STATION_ZONES[station.to_s]["zone"]
   end
 end
